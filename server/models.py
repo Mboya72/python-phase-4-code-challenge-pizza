773eaf4e5d8a87e95f6e1f302936b2ef7ddb8ae3
Mboya72 <elvindio72@gmail.com>
2025-01-18 21:33:21 +0300

    debbuging my models.py

--- server/models.py
+++ server/models.py
@@ -20,13 +20,13 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
+    
     restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
     def to_dict(self):
-    
         return {
             'id': self.id,
             'name': self.name,
@@ -42,6 +42,7 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
+    
     restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
 
     def __repr__(self):
@@ -64,15 +65,16 @@ class RestaurantPizza(db.Model, SerializerMixin):
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
 
-    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas', cascade='all, delete-orphan')
+    
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas', cascade='all, delete-orphan', single_parent=True)
     pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
+    
     @validates('price')
     def validate_price(self, key, price):
         if not (1 <= price <= 30):
             raise ValueError("Price must be between 1 and 30")
         return price
-
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
